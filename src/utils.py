79dcdc7066e45faf77255397b068d98f48661902
Adityakumar9631 <adityakrgolu7488@gmail.com>
2025-10-02 23:52:44 +0530

    HyperParameter Tunning

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,14 @@
 import os
 import sys
-import dill
-import numpy as np
+
+import numpy as np 
 import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-from sklearn.metrics import r2_score
-
-
-
 
 def save_object(file_path, obj):
     try:
@@ -17,23 +17,26 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param=None):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            
+            para=param[list(models.keys())[i]]
 
-            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -49,3 +52,4 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param=None):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
